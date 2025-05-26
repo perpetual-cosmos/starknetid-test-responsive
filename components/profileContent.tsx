4480d8389e86c0e34e0a5716631e5c8bdd8aa9a0
perpetual-cosmos <prataptarunsingh241@gmail.com>
2025-05-26 14:19:38 +0530

    correct profile

--- components/profileContent.tsx
+++ components/profileContent.tsx
@@ -1,4 +1,7 @@
 import { Clock } from "@deemlol/next-icons";
+import { Github } from "@deemlol/next-icons";
+import { Discord } from "@deemlol/next-icons";
+import { Twitter } from "@deemlol/next-icons";
 const ProfileContenet = () => {
   return (
     <div className="bg-[#FFFCF8] h-[50vh] w-[50vw] rounded-2xl shadow-xl">
@@ -10,10 +13,10 @@ const ProfileContenet = () => {
         </div>
         <h1 className="text-xl font-bold">KEVILS.STARK</h1>
         <p>0x4fdf...i8f8</p>
-        <div className="flex mt-8">
-        <div className="ml-[4vw] flex items-center bg-white p-2 shadow-sm rounded"><Clock size={18} color="#000000" className="border-0 mr-1"/><button className="mr-2">Starknet Id</button></div>
-        <div className="ml-[4vw] flex items-center bg-white p-2 shadow-sm roudned"><Clock size={18} color="#000000" className="border-0 mr-1"/><button className="mr-2">Starknet Id</button></div>
-        <div className="ml-[4vw] flex items-center bg-white p-2 shadow-sm rounded"><Clock size={18} color="#000000" className="border-0 mr-1"/><button className="mr-2">Starknet Id</button></div>
+        <div className="flex mt-8 font-bold text-sm">
+        <div className="ml-[4vw] flex items-center bg-white p-2 shadow-sm rounded"><Twitter size={18} color="#000000" className="border-0 mr-1"/><button className="mr-2">@KEVINSILVEE</button></div>
+        <div className="ml-[4vw] flex items-center bg-white p-2 shadow-sm roudned"><Discord size={18} color="#000000" className="border-0 mr-1"/><button className="mr-2">KEVINSILVEd</button></div>
+        <div className="ml-[4vw] flex items-center bg-white p-2 shadow-sm rounded"><Github size={18} color="#000000" className="border-0 mr-1"/><button className="mr-2">KEVILS</button></div>
         </div>
       </div>
     </div>
