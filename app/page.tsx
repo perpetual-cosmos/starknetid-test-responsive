82acce02a04a8a396e1e9c0b4bc32fe1a18b9afb
perpetual-cosmos <prataptarunsingh241@gmail.com>
2025-05-26 14:45:52 +0530

    added tree

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
-import ProfileContenet from "../components/profileContent"
+import ProfileContenet from "../components/profileContent";
 export default function Home() {
   return (
-   <div className="flex justify-center items-center h-[80vh]">
-    <ProfileContenet/>
-    </div>
+    <>
+      <div className="relative flex justify-center items-center h-[80vh]">
+        <img
+          src="/tree.png"
+          alt="tree-left"
+          className="absolute bottom-[-100] left-[-250] h-full max-h-[600px] object-contain pointer-events-none select-none -z-10"
+        />
+        <ProfileContenet />
+        <img
+          src="/tree.png"
+          alt="tree-right"
+          className="absolute bottom-[-100] right-[-250] h-full max-h-[600px] object-contain pointer-events-none select-none -z-10"
+        />
+      </div>
+    </>
   );
 }
