337432d81f97f01219397d8024cd5ff65b2b63cd
perpetual-cosmos <prataptarunsingh241@gmail.com>
2025-05-26 13:00:14 +0530

    css learning repo

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import ProfileContenet from "../components/profileContent"
+export default function Home() {
+  return (
+   <div className="flex justify-center items-center h-[80vh]">
+    <ProfileContenet/>
+    </div>
+  );
+}
---
+++ components/profileContent.tsx
@@ -0,0 +1,9 @@
+const ProfileContenet = () => {
+  return (
+    <div className="bg-white h-[50vh] w-[50vw] rounded-2xl">
+      
+    </div>
+  )
+};
+
+export default ProfileContenet;
