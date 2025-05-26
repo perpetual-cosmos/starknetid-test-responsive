33548865463ad433299541b9a551804ecb516190
perpetual-cosmos <prataptarunsingh241@gmail.com>
2025-05-26 14:12:19 +0530

    profile card made

--- components/header.tsx
+++ components/header.tsx
@@ -3,12 +3,12 @@ import { Clock } from "@deemlol/next-icons";
 const Header = () => {
   return (
     <div className="flex justify-between py-6">
-        <div className="ml-[4vw] flex items-center bg-white p-2 "><Clock size={18} color="#000000" className="border-0 mr-1"/><button className="mr-2">Starknet Id</button></div>
+        <div className="ml-[4vw] flex items-center bg-white p-2 shadow-sm rounded"><Clock size={18} color="#000000" className="border-0 mr-1"/><button className="mr-2">Starknet Id</button></div>
         <div className="mr-[4vw] flex gap-x-4">
             <button>My identity</button>
             <button>Domains</button>
             <button><AlignJustify size={24} color="#000000" /></button>
-            <div className="flex items-center bg-white p-2 rounded"><button className="mr-2">kevin.stark</button><Clock size={18} color="#000000" className="border-0"/></div>
+            <div className="flex items-center bg-white p-2 shadow-sm rounded"><button className="mr-2">kevin.stark</button><Clock size={18} color="#000000" className="border-0"/></div>
         </div>
     </div>
   )
--- components/profileContent.tsx
+++ components/profileContent.tsx
@@ -1,9 +1,23 @@
+import { Clock } from "@deemlol/next-icons";
 const ProfileContenet = () => {
   return (
-    <div className="bg-white h-[50vh] w-[50vw] rounded-2xl">
-      
+    <div className="bg-[#FFFCF8] h-[50vh] w-[50vw] rounded-2xl shadow-xl">
+      <div className="flex flex-col items-center">
+        <div className="flex justify-center my-8 mx-4">
+          <div className="w-32 h-32 rounded-full overflow-hidden bg-black">
+            <img src="./strid.jpg" alt="profile image" />
+          </div>
+        </div>
+        <h1 className="text-xl font-bold">KEVILS.STARK</h1>
+        <p>0x4fdf...i8f8</p>
+        <div className="flex mt-8">
+        <div className="ml-[4vw] flex items-center bg-white p-2 shadow-sm rounded"><Clock size={18} color="#000000" className="border-0 mr-1"/><button className="mr-2">Starknet Id</button></div>
+        <div className="ml-[4vw] flex items-center bg-white p-2 shadow-sm roudned"><Clock size={18} color="#000000" className="border-0 mr-1"/><button className="mr-2">Starknet Id</button></div>
+        <div className="ml-[4vw] flex items-center bg-white p-2 shadow-sm rounded"><Clock size={18} color="#000000" className="border-0 mr-1"/><button className="mr-2">Starknet Id</button></div>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default ProfileContenet;
